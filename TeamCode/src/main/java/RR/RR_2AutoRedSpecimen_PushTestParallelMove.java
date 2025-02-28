cbdd3bfe319418afa5f1e05758f719470b523734
MarcusKoT <115203248+MarcusKoT@users.noreply.github.com>
2025-02-28 17:30:04 -0500

    debug program 2/23/2025: create RED TeleOp by extending class 2/22/2025: Dominic's 3 auto and rename to Q1... 2/22/2025 Need to include Dominic auto code 2/21/2025 OT at Rodridriguez: 4 specimen Auto ready MT: manual outtake slider control and zero reset by PS button
    2/21/2025: shorten branch name, label as latest to start 2/21/2025
    2/21/2025:
    ER updated of Red Basket
    WT/ER add and revised of remote work on 2/20/2025 evening
    
    2/20/2025
    add update Dominic Feb19 Auto Basket
    add DataLogging to initloop() and loop() in BLUE_TeleOpV1
    
    2/19/2025:
    TeleOp: telemetry update;
    Pedro Auto: timeout and Point.CARTESIAN
    
    2/17/2025: partially tune PedroPathing.
    2/17/2025: 5 specimen auto--started, need 2 more seconds
    2/17/2025 tuned RR, test 4 specimen auto
    2/17/2025: add Pinpoint and OTOS to RR and PedroPathing--need to test and tune next
    
    2/16:
    copy/paste from email 2/10/2025 for RR_2AutoRedSpecimen_PushTestParallelMove and for MeepMeepTesting_2bots_OT
    Pedro_AutoRedSpecimen1 done--diagonal curve path to submersible and spline curve on return
    
    2/16: version 3: MT: reactivate color sensor again, inside Telemetry color method, change to red, blue, yellow value and not reading eachtime.
      WT: update Pedro AutoRedSpecimen1 to have curvaPath ver1
    
    2/16: version 2: MT/WT: move robot.Sensor.colorIntake.setGain(colorGain) to init() only
    
    2/16: MT move led program into telemetry method to attempt to fix lag
    WT: update to pedro pathing 1.0.8
    1.17.2025: Dom, oliver auto
    1/16/2025: OT start with Auto--use intake to take sample to home
    
    1/13/2025: MT update teleOp
               OT--path test of RedSpec
    
    1/11/2025: OT testing method in AutoCore vs. using LinearOp mode directed with extending a common program
    
    1/6/2025 MT: Axon outtake arm setup
    1/5/2025: MT--tune claw servo and outtake extension
    to be done: Axon outtake arm
    
    Merge remote-tracking branch 'origin/master_Jan3latest' into master_Jan3latest
    
    Merge remote-tracking branch 'origin/master_Feb17Latest_OT' into master_Feb17Latest_OT

--- TeamCode/src/main/java/RR/RR_2AutoRedSpecimen_PushTestParallelMove.java
+++ TeamCode/src/main/java/RR/RR_2AutoRedSpecimen_PushTestParallelMove.java
@@ -103,9 +103,9 @@ public class RR_2AutoRedSpecimen_PushTestParallelMove extends LinearOpMode {
         double clawOpen_Pos = 0.0;  // change from Eduardo 2/17  0.7;       //open claw after score
         double clawOpen_pauseTimeSecond = 0.2;
 
-        double wallPickup_OuttakeArmAxonPos = 0.28;         //rotate outtake arm into robot to wall pickup position
+        double wallPickup_OuttakeArmAxonPos = 0.29;         //rotate outtake arm into robot to wall pickup position
         int wallPickup_SliderPos = 0;                       //outtake slider position to pickup specimen from wall
-        double wallPickup_OuttakeExtensionPos =  0.85;      //extend out arm out (need to place this to the place after preloadscore)
+        double wallPickup_OuttakeExtensionPos =  0.82;      //extend out arm out (need to place this to the place after preloadscore)
 
 
         /** START */
@@ -130,11 +130,11 @@ public class RR_2AutoRedSpecimen_PushTestParallelMove extends LinearOpMode {
                 .setReversed(true)
                 .strafeTo(new Vector2d(44,-7), velFast, accMedium)                          //strafe from midway to Sample4
 
-                .strafeTo(new Vector2d(47, -50), velFast, accFast)      //pushSample4Home
+                .strafeTo(new Vector2d(48, -50), velFast, accFast)      //pushSample4Home
 
                 .setReversed(true)
-                .strafeTo(new Vector2d(54, -8), velFast, accFast)       //toward Sample5
-                .strafeTo(new Vector2d(55, -50), velFast, accMedium)    //pushSample5Home
+                .strafeTo(new Vector2d(55, -8), velFast, accFast)       //toward Sample5
+                .strafeTo(new Vector2d(56, -50), velFast, accMedium)    //pushSample5Home
 
 //                .setReversed(true)
 //                .strafeTo(new Vector2d(62, -8), velFast, accMedium)     //toward Sample6
@@ -346,7 +346,7 @@ public class RR_2AutoRedSpecimen_PushTestParallelMove extends LinearOpMode {
                                 //autoOuttakeSliderAction(wallPickup_SliderPos, 1),               //=0 outtake slider position to pickup specimen from wall
                                 collectSpec2.build()  //open claw; move back from submersible; delay rotate arm in, extend to wall, lower slider; THEN goto wall
                         ),
-                        autoClawAction(1, clawClose_pauseTimeSecond),                     //close claw
+                        autoClawAction(clawClose_Pos, clawClose_pauseTimeSecond),                     //close claw
                         new ParallelAction(
                                 autoOuttakeSliderAction(scoring_OutakeSlider_ScoringPos, 1),        //move slider up
                                 autoouttakeExtensionAction(scoring_OuttakeExtension_ScoringPos),          //bring arm into robot to scoring position
